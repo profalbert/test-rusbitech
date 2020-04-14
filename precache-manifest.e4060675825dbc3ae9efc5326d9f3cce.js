8979f9fc4ba80729ffb528cb992001c5d9af7a28
Albert <alb.adm.ru@gmail.com>
2020-04-14 13:28:41 +0300

    Updates

---
+++ precache-manifest.e4060675825dbc3ae9efc5326d9f3cce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9b734a0e357a1924a07048a5123449",
+    "url": "/test-rusbitech/index.html"
+  },
+  {
+    "revision": "f35aa8780ff0607839a4",
+    "url": "/test-rusbitech/static/css/main.70008a87.chunk.css"
+  },
+  {
+    "revision": "0f4296b193d224d8593c",
+    "url": "/test-rusbitech/static/js/2.40632d34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-rusbitech/static/js/2.40632d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35aa8780ff0607839a4",
+    "url": "/test-rusbitech/static/js/main.a85834a8.chunk.js"
+  },
+  {
+    "revision": "fb0d702eed69faab6981",
+    "url": "/test-rusbitech/static/js/runtime-main.5133ac55.js"
+  }
+]);
